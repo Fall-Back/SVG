124f0172fca8b5f8a3c9175d8205a05757b21217
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2015-05-19 16:51:52 +0100

    Quit before onload if unsupported

--- js/svg.js
+++ js/svg.js
@@ -2,35 +2,37 @@
     Fix CSS shortcomings in older browsers. Specifically fix the width of the objects container
     to be the same width as the object.
 */
-window.onload = (function(onload) {
-    return function(event) {
-        if (!(
-            document.body.style.msTouchSelect !== undefined /* IE10+ */
-         || (document.all && document.addEventListener && !window.atob) /* IE9 */
-         || Object.prototype.toString.call(window.operamini) === '[object OperaMini]' /* Opera Mini */
-        )) {
-            return;
-        }
-        console.log('Applying inline-block fix');
 
-        var objs = document.getElementsByTagName('object');
-        var i = 0
-          , l = objs.length
-          , data
-          , obj;
+if (!!(
+    document.body.style.msTouchSelect !== undefined /* IE10+ */
+ || (document.all && document.addEventListener && !window.atob) /* IE9 */
+ || Object.prototype.toString.call(window.operamini) === '[object OperaMini]' /* Opera Mini */
+)) {
 
-        for (i; i < l; i++) {
-            obj = objs[i];
-            // Check object is inside of an .svg__link and
-            // not inside of an .svg--fluid
-            if (obj.parentNode.className.indexOf('svg__link') == -1 || obj.parentNode.parentNode.className.indexOf('svg--fluid') >= 0) {
-                continue;
+    //console.log('Applying inline-block fix');
+
+    window.onload = (function(onload) {
+        return function(event) {
+
+            var objs = document.getElementsByTagName('object');
+            var i = 0
+              , l = objs.length
+              , data
+              , obj;
+
+            for (i; i < l; i++) {
+                obj = objs[i];
+                // Check object is inside of an .svg__link and
+                // not inside of an .svg--fluid
+                if (obj.parentNode.className.indexOf('svg__link') == -1 || obj.parentNode.parentNode.className.indexOf('svg--fluid') >= 0) {
+                    continue;
+                }
+                obj.style.maxWidth          = 'none';
+                obj.style.minWidth          = '0';
+                obj.parentNode.style.width  = '100%';
+                obj.parentNode.style.width  = obj.offsetWidth + 'px';
+                obj.style.maxWidth          = '100%';
             }
-            obj.style.maxWidth          = 'none';
-            obj.style.minWidth          = '0';
-            obj.parentNode.style.width  = '100%';
-            obj.parentNode.style.width  = obj.offsetWidth + 'px';
-            obj.style.maxWidth          = '100%';
         }
-    }
-}(window.onload));
+    }(window.onload));
+}
